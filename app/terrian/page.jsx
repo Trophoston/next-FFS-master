e6f3379a15607bbb3d3a5003acf84dc35b62ca6b
Trophoston <95759773+Trophoston@users.noreply.github.com>
2024-07-30 16:20:15 +0000

    update terrian page

--- app/terrian/page.jsx
+++ app/terrian/page.jsx
@@ -3,10 +3,11 @@ import { faCheck, faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
 
 export default function terrian() {
   return (
-   <>
-    <h1>terrian.jsx</h1>
-    <FontAwesomeIcon icon={faBuildingColumns} className="fa-10x" />
-    <FontAwesomeIcon icon={faCheck} style={{ color: "red" }}/>
-   </>
+    <>
+    <p className='text-center text-5xl pb-2 text-[#2A5B3E] font-extrabold drop-shadow-lg'>แผนที่ภูมิศาสตร์</p>
+    <div className="rounded-2 overflow-hidden flex flex-col justify-center items-center ">
+    <iframe className="w-full lg:w-5/6 p-2 h-[85vh]"frameborder="0"  marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=297&amp;hl=en&amp;q=%20thailand+()&amp;t=k&amp;z=6&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    </div>
+  </>
   );
 }
